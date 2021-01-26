f32c77e1da331d9de540d7fbd51f51fbde1a4865
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2021-01-25 22:02:17 -0500

    Added a comment or two

--- educational_material/sem3/slides/build.sh
+++ educational_material/sem3/slides/build.sh
@@ -4,8 +4,8 @@ for i in {01..10}; do
     infile=$i.md
     outfile=$i.pptx
 
-    if [ -f $infile ]; then
-        if [ $infile -nt $outfile ]; then
+    if [ -f $infile ]; then  # XX.md exists and is a file
+        if [ $infile -nt $outfile ]; then  # XX.md is newer than XX.pptx
             echo "Converting $infile to $outfile..."
             pandoc --from gfm --to pptx --output $outfile --slide-level 2 $infile
         else
