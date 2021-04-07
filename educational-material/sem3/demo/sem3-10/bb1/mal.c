85c77dd591ae1ac441f3999d7445d25a6df61c52
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-04-07 15:43:29 -0400

    Pedantic C code

--- educational-material/sem3/demo/sem3-10/bb1/mal.c
+++ educational-material/sem3/demo/sem3-10/bb1/mal.c
@@ -5,25 +5,25 @@
  * Print exactly n characters from s to stdout, regardless if they are
  * printable.
  */
-void print_bytes(char *s, size_t n);
+void print_bytes(char const *s, size_t n);
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-    char access_code[] = "Sup3rs3cr3tC0de";
+    char access_code[16] = "Sup3rs3cr3tC0de";
     char padding[32];
-    char rbp[] = "\x90\xde\xff\xff\xff\x7f\x00\x00";
-    char win[] = "\x72\x11\x40\x00\x00\x00\x00\x00";
+    char rbp[8] = "\x90\xde\xff\xff\xff\x7f\x00\x00";
+    char win[8] = "\x72\x11\x40\x00\x00\x00\x00\x00";
 
     print_bytes(access_code, sizeof(access_code));
     print_bytes(padding, sizeof(padding));
-    print_bytes(rbp, sizeof(rbp) - 1);
-    print_bytes(win, sizeof(win) - 1);
+    print_bytes(rbp, sizeof(rbp));
+    print_bytes(win, sizeof(win));
     fputc('\n', stdout);
 
     return EXIT_SUCCESS;
 }
 
-void print_bytes(char *s, size_t n)
+void print_bytes(char const *s, size_t n)
 {
     size_t i;
 
